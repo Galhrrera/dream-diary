9a8d2bb79f77e429ad891522580d7d2d0cde12ed
galhrrera <juan.galeanoherrera@gmail.com>
2023-08-21 11:21:10 -0500

    Avances en lista de sueños

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,14 @@ const recordDream = document.getElementById("record-dream-section");
 const savedDream = document.getElementById("saved-dream-section");
 const dreamDetails = document.getElementById("dream-details-section");
 const dreamList = document.getElementById("dream-list-section");
+
+//Referencias header
 const header = document.getElementById("header");
 const headerLink = document.getElementById("headerLink");
+
+//Referencias botones
+const startToDreamListBtn = document.getElementById("dreamListBtn");
+const startToRecordDreamBtn = document.getElementById("recordDreamBtn");
 
 
 window.onload = function () {
@@ -17,22 +23,38 @@ window.onload = function () {
 }
 
 function hideAllSections() {
-    start.classList.toggle("hidden");
-    recordDream.classList.toggle("hidden");
-    savedDream.classList.toggle("hidden");
-    dreamDetails.classList.toggle("hidden");
-    dreamList.classList.toggle("hidden");
-    header.classList.toggle("hidden");
+    start.classList.add("hidden");
+    recordDream.classList.add("hidden");
+    savedDream.classList.add("hidden");
+    dreamDetails.classList.add("hidden");
+    dreamList.classList.add("hidden");
+    header.classList.add("hidden");
 }
 
 function hideSplahs(){
-    splash.classList.toggle("hidden");
-    start.classList.toggle("hidden");
-    header.classList.toggle("hidden");
+    splash.classList.add("hidden");
+    start.classList.remove("hidden");
+    header.classList.remove("hidden");
 }
 
+//CORREGIR MÉTODO
 headerLink.addEventListener("click", () => {
     hideAllSections();
     startSection.classList.remove("hidden");
+});
+
+//Start to List section
+document.addEventListener('DOMContentLoaded', function () {
+    
+    startToDreamListBtn.addEventListener('click', function () {
+        // Remover la clase actual y agregar la nueva clase
+        // elemento1.classList.toggle('clase1');
+        // elemento1.classList.toggle('clase2');
+        
+        // elemento2.classList.toggle('clase2');
+        // elemento2.classList.toggle('clase1');
+        start.classList.toggle("hidden");
+        dreamList.classList.toggle("hidden");
+    });
 });
 
